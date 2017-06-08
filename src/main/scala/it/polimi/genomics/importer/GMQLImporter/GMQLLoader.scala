e040ffc41b69f824a699aa8811301b6eb04ebd3e
acanakoglu <acanak@gmail.com>
2017-06-08 18:19:45 +0200

    select repository type from repository.xml

--- src/main/scala/it/polimi/genomics/importer/GMQLImporter/GMQLLoader.scala
+++ src/main/scala/it/polimi/genomics/importer/GMQLImporter/GMQLLoader.scala
@@ -5,15 +5,17 @@ import java.util
 
 import it.polimi.genomics.core.DataStructures.IRDataSet
 import it.polimi.genomics.importer.FileDatabase.{FileDatabase, STAGE}
-import it.polimi.genomics.repository.FSRepository.LFSRepository
-import it.polimi.genomics.repository.GMQLSample
+import it.polimi.genomics.repository.{GMQLRepository, GMQLSample, Utilities}
 import org.slf4j.LoggerFactory
+
 /**
   * Created by Nacho on 10/17/16.
   */
 class GMQLLoader {
   val logger = LoggerFactory.getLogger(this.getClass)
-  val repo = new LFSRepository
+  val ut: Utilities = Utilities()
+  val repo: GMQLRepository = ut.getRepository()
+
   /**
     * using information in the information, will insert into GMQLRepository the files
     * already downloaded, transformed and organized.
@@ -28,46 +30,47 @@ class GMQLLoader {
     * @param source contains files location and datasets organization
     */
   def loadIntoGMQL(source: GMQLSource): Unit = {
-    val gmqlUser = source.parameters.filter(_._1=="gmql_user").head._2
+    val gmqlUser = source.parameters.filter(_._1 == "gmql_user").head._2
+    repo.registerUser(gmqlUser)
     val stage = STAGE.TRANSFORM
     logger.info("Preparing for loading datasets into GMQL")
-    source.datasets.foreach(dataset =>{
-      logger.debug("dataset "+dataset.name)
-      if(dataset.loadEnabled) {
+    source.datasets.foreach(dataset => {
+      logger.debug("dataset " + dataset.name)
+      if (dataset.loadEnabled) {
         val path = source.outputFolder + File.separator + dataset.outputFolder + File.separator + "Transformations"
 
         val listAdd = new java.util.ArrayList[GMQLSample]()
 
-        val datasetId = FileDatabase.datasetId(FileDatabase.sourceId(source.name),dataset.name)
+        val datasetId = FileDatabase.datasetId(FileDatabase.sourceId(source.name), dataset.name)
 
-        FileDatabase.getFilesToProcess(datasetId,stage).filter(_._2.endsWith(".meta")).foreach(file => {
-          val fileName = if(file._3 == 1) file._2 else file._2.replaceFirst("\\.","_"+file._3+".")
+        FileDatabase.getFilesToProcess(datasetId, stage).filter(_._2.endsWith(".meta")).foreach(file => {
+          val fileName = if (file._3 == 1) file._2 else file._2.replaceFirst("\\.", "_" + file._3 + ".")
           try {
             listAdd.add(GMQLSample(
               path + File.separator + fileName.substring(0, fileName.lastIndexOf(".meta")),
               path + File.separator + fileName,
               null))
-          }catch {
-            case e: Throwable => logger.warn("data or metadata files missing: "+ path + File.separator + fileName + ". more details: "+e.getMessage)
+          } catch {
+            case e: Throwable => logger.warn("data or metadata files missing: " + path + File.separator + fileName + ". more details: " + e.getMessage)
           }
         })
 
         if (listAdd.size() > 0) {
           //CHANGE GMQLUSER TO SOURCE PARAMETERS
-          logger.info("Trying to add " + dataset.name +" to user: "+ gmqlUser)
+          logger.info("Trying to add " + dataset.name + " to user: " + gmqlUser)
           val datasetName =
-            if(dataset.parameters.exists(_._1=="loading_name"))
-              dataset.parameters.filter(_._1=="loading_name").head._2
+            if (dataset.parameters.exists(_._1 == "loading_name"))
+              dataset.parameters.filter(_._1 == "loading_name").head._2
             else
               source.name + "_" + dataset.name
           //if repo exists I do DELETE THEN ADD
-          if(dsExists(gmqlUser,datasetName))
-          try{
-            repo.deleteDS(datasetName,gmqlUser)
-          }catch {
-            //should be GMQLDSNotFound but dont know yet where it is.
-            case e:Exception => logger.info("Dataset " + datasetName + " is not defined before!!")
-          }
+          if (dsExists(gmqlUser, datasetName))
+            try {
+              repo.deleteDS(datasetName, gmqlUser)
+            } catch {
+              //should be GMQLDSNotFound but dont know yet where it is.
+              case e: Exception => logger.info("Dataset " + datasetName + " is not defined before!!")
+            }
 
           try {
             repo.importDs(
@@ -75,24 +78,25 @@ class GMQLLoader {
               gmqlUser,
               listAdd,
               path + File.separator + dataset.name + ".schema")
-            logger.info("import for dataset "+dataset.name+" completed")
+            logger.info("import for dataset " + dataset.name + " completed")
           }
           catch {
             case e: Throwable => logger.error("import failed: " + e.getMessage)
           }
         }
         else
-          logger.info("dataset "+dataset.name+" has no files to be loaded")
+          logger.info("dataset " + dataset.name + " has no files to be loaded")
       }
       else
-        logger.debug("dataset "+dataset.name+" not included to load.")
+        logger.debug("dataset " + dataset.name + " not included to load.")
     })
   }
-  def dsExists (username: String, datasetName: String ): Boolean ={
+
+  def dsExists(username: String, datasetName: String): Boolean = {
     val dss: util.List[IRDataSet] = repo.listAllDSs(username)
     var exists = false
-    for(i <- 0 until dss.size()){
-      if(dss.get(i).position == datasetName)
+    for (i <- 0 until dss.size()) {
+      if (dss.get(i).position == datasetName)
         exists = true
     }
     exists
