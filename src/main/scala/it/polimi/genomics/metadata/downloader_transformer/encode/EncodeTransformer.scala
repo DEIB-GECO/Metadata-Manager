0e12e5e22e32a3c31e558052bccd5e5d284b67db
acanakoglu <acanak@gmail.com>
2018-12-05 15:16:12 +0100

    escape the values which contains newline or tab character

--- src/main/scala/it/polimi/genomics/metadata/downloader_transformer/encode/EncodeTransformer.scala
+++ src/main/scala/it/polimi/genomics/metadata/downloader_transformer/encode/EncodeTransformer.scala
@@ -3,17 +3,17 @@ package it.polimi.genomics.metadata.downloader_transformer.encode
 import java.io.{File, _}
 import java.util
 
+import it.polimi.genomics.metadata.database.{FILE_STATUS, FileDatabase, Stage}
 import it.polimi.genomics.metadata.downloader_transformer.Transformer
 import it.polimi.genomics.metadata.downloader_transformer.default.utils.Unzipper
-import it.polimi.genomics.metadata.database.{FILE_STATUS, FileDatabase, Stage}
-import it.polimi.genomics.metadata.step.xml.Dataset
 import it.polimi.genomics.metadata.step.xml
+import it.polimi.genomics.metadata.step.xml.Dataset
 import org.codehaus.jackson.map.MappingJsonFactory
 import org.codehaus.jackson.{JsonNode, JsonParser, JsonToken}
 import org.slf4j.LoggerFactory
 
+import scala.collection.JavaConverters._
 import scala.io.Source
-import collection.JavaConverters._
 
 
 /**
@@ -237,9 +237,9 @@ class EncodeTransformer extends Transformer {
           if (gmqlSource.parameters.exists(_._1 == "multiple_comma_separated") &&
             gmqlSource.parameters.filter(_._1 == "multiple_comma_separated").exists(_._2 == header(i)))
             for (value <- fields(i).split(", "))
-              writer.write(header(i) + "\t" + value + "\n")
+              writer.write(header(i) + "\t" + escapeCharacters(value) + "\n")
           else
-            writer.write(header(i) + "\t" + fields(i) + "\n")
+            writer.write(header(i) + "\t" + escapeCharacters(fields(i)) + "\n")
         }
       }
       writer.close()
@@ -410,7 +410,7 @@ class EncodeTransformer extends Transformer {
     }
   }
 
-    /**
+  /**
     * gets the "hard coded" exclusion categories, meant to be used for the particular cases
     * Files and Replicates should be always be there, other exclusions are managed from xml file with regex.
     */
@@ -442,7 +442,7 @@ class EncodeTransformer extends Transformer {
     if (node.isValueNode && node.asText() != ""
       && !metaList.contains(node.asText(), parents)
     ) {
-      writer.write(parents + "\t" + node.asText() + "\n")
+      writer.write(parents + "\t" + escapeCharacters(node.asText()) + "\n")
       metaList.add((node.asText(), parents))
     }
     else {
@@ -471,6 +471,12 @@ class EncodeTransformer extends Transformer {
       }
     }
   }
+
+  def escapeCharacters(in: String) = in.replace('\t', ' ').replace('\n', ' ')
+
+  //other possibility escape the value
+  //  def escapeCharacters2(in:String) =  in.replace("\t", "\\t").replace("\n", "\\n")
+
 
   //  //--------------------------------------------SCHEMA SECTION----------------------------------------------------------
   //  /**
