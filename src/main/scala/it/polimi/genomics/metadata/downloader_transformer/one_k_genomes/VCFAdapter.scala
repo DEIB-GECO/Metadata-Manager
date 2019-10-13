4b2c5e85209c8d7351a4f052c689fb3fe2c35140
tomalf2 <tommaso.alfonsi@mail.polimi.it>
2019-10-13 13:31:18 +0200

    corrects the computation of mutations' end

--- src/main/scala/it/polimi/genomics/metadata/downloader_transformer/one_k_genomes/VCFAdapter.scala
+++ src/main/scala/it/polimi/genomics/metadata/downloader_transformer/one_k_genomes/VCFAdapter.scala
@@ -2,9 +2,9 @@ package it.polimi.genomics.metadata.downloader_transformer.one_k_genomes
 
 import java.io.BufferedReader
 
-import it.polimi.genomics.metadata.util.{FileUtil, RoughReadProgress}
 import it.polimi.genomics.metadata.util.vcf.VCFMutation.{ALT_MULTI_VALUE_SEPARATOR, MutationProperties, maxLengthAltAllele, splitMultiValuedInfo}
 import it.polimi.genomics.metadata.util.vcf.{VCFInfoKeys, VCFMutation}
+import it.polimi.genomics.metadata.util.{FileUtil, RoughReadProgress}
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.util.{Failure, Success}
@@ -36,33 +36,33 @@ class VCFAdapter(VCFFilePath: String) {
    * @return false if the sample doesn't appear in this VCf, true otherwise.
    */
   def appendMutationsOf(sampleName: String, toFilePath: String): Boolean = {
-    biosamples.indexOf(sampleName) match {
-      case -1 => // the sample isn't listed in this VCF
-        false
-      case index =>
-        val writer = FileUtil.writeAppend(toFilePath, startOnNewLine = true).get  // let it throw exception if failed
-        val reader = FileUtil.open(VCFFilePath).get    // let it throw exception if failed
-        advanceAndGetHeaderLine(reader)   // advance reader and skip header line
-        FileUtil.scanFileAndClose(reader, mutationLine => {
-          val mutation = new VCFMutation(mutationLine)
-          val formatOfSample = mutation.format(sampleName, biosamples)
-          if(formatOfSample.isMutated){
-            val infoFields = mutation.info
-            writer.write(OneKGTransformer.tabber(
-              mutation.chr,
-              mutation.pos,
-              //TODO mutation.end
-              "+",   // strand (see class documentation for explanations)
-              mutation.id,
-              mutation.ref,
-              mutation.alt,
-              infoFields.getOrElse(VCFInfoKeys.ALLELE_FREQUENCY, MISSING_VALUE_CODE)
-              //TODO go on with other attributes based on the desired schema for region data
-            ))
-          }
-        }, setupReadProgressCanary(VCFFilePath))
-        writer.close()
-        true
+    val index = biosamples.indexOf(sampleName)
+    if(index == -1)
+      false // the sample isn't listed in this VCF
+    else {
+      val writer = FileUtil.writeAppend(toFilePath, startOnNewLine = true).get  // let it throw exception if failed
+      val reader = FileUtil.open(VCFFilePath).get    // let it throw exception if failed
+      advanceAndGetHeaderLine(reader)   // advance reader and skip header line
+      FileUtil.scanFileAndClose(reader, mutationLine => {
+        val mutation = new VCFMutation(mutationLine)
+        val formatOfSample = mutation.format(sampleName, biosamples)
+        if(formatOfSample.isMutated){
+          val infoFields = mutation.info
+          writer.write(OneKGTransformer.tabber(
+            mutation.chr,
+            mutation.pos,
+            //TODO mutation.end
+            "+",   // strand (see class documentation for explanations)
+            mutation.id,
+            mutation.ref,
+            mutation.alt,
+            infoFields.getOrElse(VCFInfoKeys.ALLELE_FREQUENCY, MISSING_VALUE_CODE)
+            //TODO go on with other attributes based on the desired schema for region data
+          ))
+        }
+      }, setupReadProgressCanary(VCFFilePath))
+      writer.close()
+      true
     }
   }
 
@@ -110,7 +110,7 @@ class VCFAdapter(VCFFilePath: String) {
   }
 
   /**
-   * Prepares an instance of {@link RoughReadProgress} to receive progress updates while processing files very large in size.
+   * Prepares an instance of RoughReadProgress to receive progress updates while processing files very large in size.
    * Progress are not logged through a logger instance as they're useful only for a user who wants to have an estimate
    * of the time left to finish.
    * This estimate is just a rough approximation and requires some more seconds before the file processing can begin in
@@ -119,7 +119,7 @@ class VCFAdapter(VCFFilePath: String) {
   private def setupReadProgressCanary(fullFilePath: String): Option[RoughReadProgress] ={
     println("COUNTING LINES OF FILE: "+fullFilePath)
     FileUtil.countLines(fullFilePath) match {
-      case Failure(exception) =>
+      case Failure(_) =>
         println("COUNT OF LINES IN FILE FAILED")
         None
       case Success(value) =>
@@ -225,12 +225,17 @@ object VCFAdapter {
      *         For mobile element SVs, it relies on the presence of the INFO attribute "SVLEN" or "MEINFO" with
      *         well defined START and END sub-properties.
      *         For all the other SVs, it uses "SVLEN" if present or "END" INFO attributes.
+     *         If none of the above cases is positive and SVTYPE is "ALU", then an approximate length of 300 bases is returned.
      *         If nothing of the above strategies works, end has the same value of the starting pos.
      */
     def end:Long ={
       length+m.pos.toLong
     }
 
+    /**
+     * Since length computation can be expensive, it's saved on first access.
+     * For details about length computation see method "end" of implicit class OKGMutation
+     */
     private def _length:Long ={
       import VCFInfoKeys._
         // INDELs
@@ -238,15 +243,16 @@ object VCFAdapter {
           m.info.get(SV_TYPE).isEmpty && m.ref.length != maxLengthAltAllele(m.alt))
         // In INDELS, ALT and m.ref share the first or the last base. Moreover, ALT can have more than one ALT sequence comma separated
           Math.max(m.ref.length, m.alt.split(ALT_MULTI_VALUE_SEPARATOR).map(_.length).max) - 1
-        // SNP
-        else if (m.info.get(VARIANT_TYPE).isDefined && m.info(VARIANT_TYPE).contains("S") /*cover also the case VT="SNP"*/ ||
-          m.info.get(SV_TYPE).isEmpty && m.ref.length == 1 && maxLengthAltAllele(m.alt) == 1)
-          1
         // MNP
         else if (m.info.get(VARIANT_TYPE).isDefined && m.info(VARIANT_TYPE).contains("M") /*cover also the case VT="MNP"*/ ||
           m.info.get(SV_TYPE).isEmpty && m.ref.length == maxLengthAltAllele(m.alt))
         // MNP mutations 're like concatenated SNPs and they don't share the first or the last base.
           m.ref.length
+        // SNP
+        else if (m.info.get(SV_TYPE).isEmpty  && /* without this condition it would accept also SVs having VARIANT_TYPE="SV" */
+          ( m.info.get(VARIANT_TYPE).isDefined && m.info(VARIANT_TYPE).contains("S") /*cover also the case VT="SNP"*/ ||
+            m.ref.length == 1 && maxLengthAltAllele(m.alt) == 1) )
+          1
         // SVs with SV_LENGTH like mobile elements and some others (DEL|DUP|INV)
         else if (m.info.get(SV_LENGTH).isDefined)
         // max of absolute values of SV_LENGTH. SV_LENGTH is negative for SVs representing long deletions
@@ -260,14 +266,24 @@ object VCFAdapter {
           try {
             parts(2).toLong - parts(1).toLong
           } catch {
-            case e: Exception =>
-              VCFMutation.logger.error("MEm.info not formatted as expected. MEINFO original value was " + m.info(MOBILE_ELEM_INFO), e)
-              0
+            case _: Exception =>
+                VCFMutation.logger.error("MEINFO unexpected format in mutation "+mutationStringWithoutFormat)
+                0
           }
+        } else if(m.info.get(SV_TYPE).isDefined && m.info(SV_TYPE)=="ALU") {  // peculiar case in hg19 chrX
+          VCFMutation.logger.info("MEINFO unexpected format in mutation "+mutationStringWithoutFormat+
+            " ALU APPROXIMATE LENGTH RETURNED: 300 BASES.")
+          300   // approximate length of ALU element. Without further info, this is the best guess.
         } else { // uncovered case
-          VCFMutation.logger.error("UNABLE TO DETERMINE THE LENGTH OF VARIANT: " + m.chr + m.pos + m.id + m.ref + m.alt + m.info)
+          VCFMutation.logger.error("UNABLE TO DETERMINE THE LENGTH OF VARIANT: "+mutationStringWithoutFormat)
           0
         }
+    }
+
+    private def mutationStringWithoutFormat: String ={
+      String.join(" ",
+        m.chr, m.pos, m.id, m.ref, m.alt, m.info.toString()
+      )
     }
 
   }
