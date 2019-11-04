71de75b66ca7d9c8084885959021e5cf74ac9acb
tomalf2 <tommaso.alfonsi@mail.polimi.it>
2019-11-04 01:12:34 +0100

    reminder about pattern for log messages when running on the server

--- src/main/scala/it/polimi/genomics/metadata/Program.scala
+++ src/main/scala/it/polimi/genomics/metadata/Program.scala
@@ -27,6 +27,7 @@ import scala.xml.{Elem, XML}
   */
 object Program extends App {
   val logger: Logger = Logger.getLogger(this.getClass)
+  //TODO restore original pattern message when pushing to the server
 //  replace original message pattern with a much shorter one to improve the readability of the output
 //    val PATTERN = "%d [%p] - %l %m%n"
   val PATTERN = "%d{HH:mm:ss} [%p]-%c{2} %m%n"
